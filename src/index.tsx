3220e658063d557c2b89a55d2b754d8e5badab1f
Tynasello <tynasello@gmail.com>
2021-08-20 18:14:23 -0400

    fixing routes

--- src/index.tsx
+++ src/index.tsx
@@ -15,7 +15,7 @@ import { HashRouter } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <HashRouter basename="/">
+    <HashRouter basename="/blog-frontend/">
       <ThemeProvider theme={Theme}>
         <GlobalStyle />
         <App />
